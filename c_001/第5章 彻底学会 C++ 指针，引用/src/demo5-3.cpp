1bc4a1613f2e4759c77f0c8c574e2f77a6ae8b64
xiaojun2010 <404869979@qq.com>
2025-03-03 00:14:35 +0800

    init

--- c_001/第5章 彻底学会 C++ 指针，引用/src/demo5-3.cpp
+++ c_001/第5章 彻底学会 C++ 指针，引用/src/demo5-3.cpp
@@ -9,11 +9,11 @@ unsigned int MAX_LEN = 11;
 
 int main() {
     char strHelloworld[] = {"helloworld"};
-    char const *pStr1 = "helloworld"; //const char*
+    char const *pStr1 = "helloworld"; //const char* ； const 修饰的字符串内容 ；pStr1 指针是允许改变的
     char *const pStr2 = strHelloworld;
     char const *const pStr3 = "helloworld"; // const char * const
-    pStr1 = strHelloworld;
-//    pStr2 = strHelloworld; //pStr2 不可改
+    pStr1 = strHelloworld; //pStr1 指向的值不变，但是 pStr1 指向可以变
+//    pStr2 = strHelloworld; //pStr2 不可改 , pStr2 指向的值可以变
 //    pStr3 = strHelloworld; //pStr3 不可改
     unsigned int len = strnlen(pStr2, MAX_LEN);
     cout << len << endl;
