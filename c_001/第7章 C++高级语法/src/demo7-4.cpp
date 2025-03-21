2b92d9a6b0142453580c4ac5a522fd8fc7834e7e
xiaojun2010 <404869979@qq.com>
2025-03-21 23:05:22 +0800

    init

--- c_001/第7章 C++高级语法/src/demo7-4.cpp
+++ c_001/第7章 C++高级语法/src/demo7-4.cpp
@@ -11,9 +11,19 @@ public:
     virtual double Area() const{
        return 0;
     }
+
+    virtual void Show() = 0;
+
+    void SetColor(int color){
+        _color = color;
+
+    }
     void Display(){
         cout << Area() <<endl;
     }
+
+private:
+    int _color;
 
 };
 
@@ -25,6 +35,10 @@ public:
     }
     double Area() const{
         return _len * _len;
+    }
+
+    void Show(){
+        cout <<" Square " <<endl;
     }
     void Display();
 
@@ -41,15 +55,39 @@ public:
     double Area() const{
         return 3.1415926 * _radius * _radius;
     }
+    void Show(){
+        cout <<" Circle " <<endl;
+    }
     void Display();
 
 private:
     double _radius;
 };
 
+class Triangle : public Shape
+{
+public:
+    Triangle(double len,double height):_len(len),_height(height){
+
+    }
+
+    void Show(){
+        cout << "Triangle" << endl;
+    }
+
+    double Area() const {
+        return 0.5 * _len * _height;
+    }
+
+private:
+    double _len;
+    double _height;
+};
+
 int main(){
 
     Square s1(2.0);
+    s1.SetColor(1);
 
     Circle c1(2.0);
 
